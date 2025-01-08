f41ab57735f8b27911117bc67f7141563ad2865f
madhav <madhavnepal916@gmail.com>
2025-01-08 15:17:11 +0545

    ..

--- news-summarizer/backend/config/config.py
+++ news-summarizer/backend/config/config.py
@@ -1,5 +1,16 @@
-MONGO_URI = "mongodb+srv://nepalmadhav32454:<db_password>@cluster0.jbm3k.mongodb.net/"
+from urllib.parse import quote_plus
+# from config import MONGO_URI,DB_NAME,COLLECTION_NAME
+
+username="nepalmadhav32454"
+password="M@dh@v2020@"
+
+
+
+encoded_username=quote_plus(username)
+encoded_password=quote_plus(password)
+
+MONGO_URI = (f"mongodb+srv://{encoded_username}:{encoded_password}@cluster0.jbm3k.mongodb.net/")
 # NEWS_API_KEY = "your_news_api_key_here"
-News_Site="cnn.com"
+# News_Site="cnn.com"
 DB_NAME = "news_db"
-COLLECTION_NAME = "summarized_news"
+COLLECTION_NAME = "raw_news"
--- news-summarizer/backend/data/processed_data.py
+++ news-summarizer/backend/data/processed_data.py
@@ -1,5 +1,10 @@
 import json
 import os
+from raw_data import fetch_articles, download_articles
+from pymongo import MongoClient
+from config.config import MONGO_URI,DB_NAME,COLLECTION_NAME
+
+
 
 cache_file = "cached_articles.json"
 
@@ -18,9 +23,25 @@ def save_articles_to_cache(articles):
         json.dump(articles, f, indent=4, default=str)
         print(f"Articles saved to {cache_file}")
 
+def insert_data_to_mongodb(data):
+    client=MongoClient(MONGO_URI)
+    db = client[DB_NAME]
+    collection = db[COLLECTION_NAME]
+
+
+    if isinstance(data,dict):
+        collection.insert_one(data)
+    elif isinstance(data,list):
+        collection.insert_many(data)
+    else:
+        print("Data format is not supported")
+    print(f"Data inserted into mongodb collection : {COLLECTION_NAME}")
+
 if __name__ == "__main__":
-    from raw_data import fetch_articles, download_articles
 
     url_dict = fetch_articles()
     article_contents = download_articles(url_dict)
     save_articles_to_cache(article_contents)
+
+
+    insert_data_to_mongodb(article_contents)
