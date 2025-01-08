c1be6e078941b918ebfa64048fef4b40c41f141c
madhav <madhavnepal916@gmail.com>
2025-01-08 15:16:24 +0545

    api code

--- news-summarizer/backend/app.py
+++ news-summarizer/backend/app.py
@@ -1,17 +1,34 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI,HTTPException
+from typing import ClassVar
+from pydantic import BaseModel
 from pymongo import MongoClient
 from config.config import MONGO_URI, DB_NAME, COLLECTION_NAME
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/news", methods=["GET"])
-def get_news():
-    client = MongoClient(MONGO_URI)
-    db = client[DB_NAME]
-    collection = db[COLLECTION_NAME]
-    articles = list(collection.find({}, {"_id": 0}))  # Exclude MongoDB _id field
-    client.close()
-    return jsonify(articles)
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
+
+
+class Article(BaseModel):
+    title=ClassVar[str]
+    text=ClassVar[str]
+    authors:list[str]
+    publish_date:list[str]
+    fetched_at:list[str]
+
+@app.route('/articles',response_model=list[Article])
+async def get_articles():
+    try:
+        articles=list(collection.find({}))
+        for article in articles:
+            article["_id"]=str(article["_id"])
+        return articles
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=str(e))
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
