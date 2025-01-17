523f8fef24208b53f1c046c46bd8bfeda87b529e
madhav <madhavnepal916@gmail.com>
2025-01-17 15:19:36 +0545

    summarizer

--- news-summarizer/backend/data/processed_data.py
+++ news-summarizer/backend/data/processed_data.py
@@ -4,6 +4,7 @@ from raw_data import fetch_articles, download_articles
 from pymongo import MongoClient
 from bson import ObjectId
 from urllib.parse import quote_plus
+import logging
 
 
 username="nepalmadhav32454"
@@ -21,38 +22,57 @@ COLLECTION_NAME = "portal_news"
 
 cache_file = "cached_articles.json"
 
+
+logging.basicConfig(
+    filename="article_storage.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+    
+    )
+
 def load_cached_articles():
     if os.path.exists(cache_file):
         try:
             with open(cache_file, "r") as f:
+                logging.info("Loading cached articles from file")
                 return json.load(f)
         except json.JSONDecodeError:
-            print("Error decoding JSON. Returning empty dictionary.")
+            logging.error("Error decoding JSON. Returning empty dictionary.")
         return {}
     return {}
 
 def save_articles_to_cache(articles):
-    with open(cache_file, "w") as f:
-        json.dump(articles, f, indent=4, default=str)
-        print(f"Articles saved to {cache_file}")
+    try:
+        with open(cache_file, "w") as f:
+            json.dump(articles, f, indent=4, default=str)
+        logging.info(f"articles saved to cached file:  {cache_file}")
+    except Exception as e:
+        logging.error(f"error saving articles to cache: {e}")
 
 def insert_data_to_mongodb(data):
-    client=MongoClient(MONGO_URI)
-    db = client[DB_NAME]
-    collection = db[COLLECTION_NAME]
+    try:
+        client=MongoClient(MONGO_URI)
+        db = client[DB_NAME]
+        collection = db[COLLECTION_NAME]
 
 
-    for source, articles in data.items():
-        for article in articles:
-            collection.insert_one(article)
-        print(f"Articles from {source} inserted into MongoDB collection: {COLLECTION_NAME}")
+        for source, articles in data.items():
+            for article in articles:
+                collection.insert_one(article)
+            logging.info(f"Articles from {source} inserted into MongoDB collection: {COLLECTION_NAME}")
+    except Exception as e:
+        logging.error(f"Error inserting data into MongoDB: {e}")
 
 
 if __name__ == "__main__":
+    try:
+        url_dict = fetch_articles()
+        article_contents = download_articles(url_dict)
+        save_articles_to_cache(article_contents)
 
-    url_dict = fetch_articles()
-    article_contents = download_articles(url_dict)
-    save_articles_to_cache(article_contents)
 
+        insert_data_to_mongodb(article_contents)
 
-    insert_data_to_mongodb(article_contents)
+        logging.info("Article fetching and storage is completed .............")
+    except Exception as main_error:
+        logging.error(f"Main error: {main_error}")
--- news-summarizer/backend/data/raw_data.py
+++ news-summarizer/backend/data/raw_data.py
@@ -2,6 +2,8 @@ import newspaper
 import time
 from datetime import datetime
 from newspaper import Article
+import logging
+import schedule
 
 paper_sources = {
     'cnn': 'https://edition.cnn.com/',
@@ -13,11 +15,18 @@ paper_sources = {
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 url_dict = {source: [] for source in paper_sources.keys()}
 total_articles = 0
-max_articles_per_source = 200
+max_articles_per_source = 30
 max_total_articles = 100
+
+logging.basicConfig(
+    filename="article_fetcher.log",
+    level=logging.INFO,
+    format="%(asctime)s-%(levelname)s-%(message)s"
+)
 
 def fetch_articles():
     global url_dict  
+    logging.info("fetching articles from source...........")
     for source_name, source_url in paper_sources.items():
         try:
             paper = newspaper.build(source_url, user_agent=user_agent, memoize_articles=False)
@@ -27,12 +36,12 @@ def fetch_articles():
                 if article.url not in url_dict[source_name]:
                     url_dict[source_name].append(article.url)
         except Exception as e:
-            print(f"Error fetching articles from {source_name}: {e}")
+            logging.error(f"Error fetching articles from {source_name}: {e}")
 
         time.sleep(2)
 
     for source, urls in url_dict.items():
-        print(f"{source}: {len(urls)}")
+        logging.info(f"{source}: {len(urls)}")
 
     return url_dict
 
@@ -40,6 +49,8 @@ def download_articles(url_dict):
     global total_articles  
     article_contents = {source: [] for source in paper_sources.keys()}
     fetched_urls = set()
+
+    logging.info("fetching article.................")
 
     while total_articles < max_total_articles:
         for source in paper_sources.keys():
@@ -73,11 +84,27 @@ def download_articles(url_dict):
 
                     time.sleep(2)
                 except Exception as e:
-                    print(f"Error fetching {url}: {e}")
+                    logging.error(f"Error fetching {url}: {e}")
 
+
+    logging.info("Finehed fetching.................")
     return article_contents
 
+def job():
+    logging.info("starting a new fetch job.....")
+    try:
+        url_dict=fetch_articles()
+        article_contents=download_articles(url_dict)
+        logging.info(f"Total articles fetched this session: {total_articles}")
+    except Exception as e:
+        logging.error(f"Error in job: {e}")
+
 if __name__ == "__main__":
-    url_dict = fetch_articles()
-    article_contents = download_articles(url_dict)
-    print(f"\nTotal articles fetched: {total_articles}")
+    logging.info("Starting the articles fetcher ......")
+    job()
+    schedule.every(1).min.do(job)
+
+
+    while True:
+        scheule.run_pending()
+        time.sleep(1)
