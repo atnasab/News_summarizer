024d3ea917d539c0f8da2623bed8faa766526c19
madhav <madhavnepal916@gmail.com>
2025-01-22 12:35:20 +0545

    messa

--- news-summarizer/backend/processed_data.py
+++ news-summarizer/backend/processed_data.py
@@ -5,20 +5,7 @@ from pymongo import MongoClient
 from bson import ObjectId
 from urllib.parse import quote_plus
 import logging
-
-
-username="nepalmadhav32454"
-password="M@dh@v2020@"
-
-
-
-encoded_username=quote_plus(username)
-encoded_password=quote_plus(password)
-
-MONGO_URI = (f"mongodb+srv://{encoded_username}:{encoded_password}@cluster0.jbm3k.mongodb.net/")
-DB_NAME = "news_data"
-COLLECTION_NAME = "portal_news"
-
+from config. config import *
 
 cache_file = "cached_articles.json"
 
